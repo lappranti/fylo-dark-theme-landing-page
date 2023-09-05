445696df730e66b4928cef45bd4e1f3f98d618d3
lappranti <ndeurys@gmail.com>
2023-09-05 16:20:47 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'fylo-dark-theme-landing-page';
+  activeMenuMobile: boolean = false;
+
+  handleToggleMenuMobile() {
+    this.activeMenuMobile = !this.activeMenuMobile;
+  }
 }
